ad0bf51c4cebcc750f3d4801eca50e7409879e6c
rahulr-1006 <80089440+rahulr-1006@users.noreply.github.com>
2024-09-03 15:35:30 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
+# __init__.py
+from flask import Flask
+from .config import Config
 
+# Initialize the Flask application
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Import the routes from main.py
+from .main import *
